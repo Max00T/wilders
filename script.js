c85f0c54f6d39258fee98d249d15aec2baa5f30f
Max00T <trousselmaxime@gmail.com>
2024-12-09 11:26:38 +0100

    partage exercice

---
+++ script.js
@@ -0,0 +1,13 @@
+import chalk from "chalk";
+
+const Lisa = "Lisa";
+const Julien = "Julien";
+const Maxence = "Maxence";
+const Kevin = "Kevin";
+
+console.log(
+	chalk.blue(Kevin),
+	chalk.green(Maxence),
+	chalk.yellow(Julien),
+	chalk.red(Lisa),
+);
